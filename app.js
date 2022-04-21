46acbbe3465d0e931ff84242bd8cfa3a2ce196bd
Liangni <light0104@gmail.com>
2022-04-21 18:04:27 +0800

    Add routes

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const app = express()
 const PORT = 3000
+const routes = require('./routes')
 
-app.get('/', (req, res) => {
-  res.send('Welcome to ChatBar!')
-})
+// 掛載總路由器
+app.use(routes)
 
 app.listen(PORT, () => {
   console.log(`App is running on port ${PORT}!`)
