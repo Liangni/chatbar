7abbf9756d63c03035ca57ff40b4d04574306647
Sin0001 <bboyliljie@gmail.com>
2022-04-21 23:35:13 +0800

    Add users register page

--- controllers/userContorller.js
+++ controllers/userContorller.js
@@ -1,6 +1,9 @@
 const userController = {
     loginPage: (req, res) => {
         res.render('users/login')
+    },
+    registerPage: (req, res) => {
+        res.render('users/register')
     }
 }
 
