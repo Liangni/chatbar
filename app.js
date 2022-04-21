4980bc87326e4a0bfcd63500f330fe46dea30e48
Liangni <light0104@gmail.com>
2022-04-21 18:30:27 +0800

    Add express-handlebars

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
 const app = express()
 const PORT = 3000
 const routes = require('./routes')
+
+// 設定樣板引擎
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs', helpers: require('./helpers/handlebars-helpers') }))
+app.set('view engine', 'hbs')
 
 // 掛載總路由器
 app.use(routes)
--- routes/modules/routes.js
+++ routes/modules/routes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) =>{
-  res.send('The request is successfully led to routes!')
+  res.render('example')
 })
 
 module.exports = router
