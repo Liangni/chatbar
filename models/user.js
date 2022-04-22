7731c0eeccc342bc728c54388c1154f31c90bf34
Liangni <light0104@gmail.com>
2022-04-22 20:04:49 +0800

    create interest model & owned-interst model

--- models/user.js
+++ models/user.js
@@ -12,6 +12,11 @@ module.exports = (sequelize, DataTypes) => {
     // associations can be defined here
     User.belongsTo(models.Gender)
     User.belongsTo(models.District)
-  };
+    User.belongsToMany(models.Interest, {
+      through: models.Owned_interest, // 透過 Owned_interests 表來建立關聯
+      foreignKey: 'UserId', // 對 Owned_interests 表設定 FK
+      as: 'CurrentInterests' // 幫這個關聯取個名稱
+    });
   return User;
+  }
 };
