9e9d44466e1940453f4f4c3b582c891104bcc45c
Liangni <light0104@gmail.com>
2022-04-25 22:45:08 +0800

    Add gender & district data to registerPage

--- controllers/userContorller.js
+++ controllers/userContorller.js
@@ -1,9 +1,19 @@
+const { Gender, District } = require('../models')
+
 const userController = {
-    loginPage: (req, res) => {
+    loginPage: (req, res) => {    
         res.render('users/login')
     },
     registerPage: (req, res) => {
-        res.render('users/register')
+        Promise.all([
+            Gender.findAll({ raw: true }),
+            District.findAll({ raw: true })
+        ])
+        .then(([genders, districts]) => {
+            res.render('users/register', { genders, districts }
+            )
+        })
+        .catch(error => console.log(error))  
     },
     getUserMessages: (req, res) => {
         res.render('userMessages')
