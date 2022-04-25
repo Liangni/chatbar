9eee123f89f2814b107fe50292e01f63f1342369
Liangni <light0104@gmail.com>
2022-04-25 14:29:44 +0800

    create district model with upgraded sequelize

--- models/user.js
+++ models/user.js
@@ -12,6 +12,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       User.belongsTo(models.Gender, { foreignKey:'genderId' })
+      User.belongsTo(models.District, { foreignKey: 'districtId' })
     }
   };
   User.init({
