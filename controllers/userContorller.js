f9a8b15f9d9345f70ee0f38ba2d9c34fc78ee952
Liangni <light0104@gmail.com>
2022-04-26 17:47:56 +0800

    refactor POST register into async await function

--- controllers/userContorller.js
+++ controllers/userContorller.js
@@ -16,58 +16,35 @@ const userController = {
         })
         .catch(err => next(err))  
     },
-    register: (req, res, next) => {
-        const { account, password, confirmPassword, genderId, birthday, occupation, districtId, interest } = req.body
+    register: async (req, res, next) => {
+        try {
+            const { account, password, confirmPassword, genderId, birthday, occupation, districtId, interest } = req.body
 
-        if (!account || !password || !confirmPassword || !genderId || !birthday || !occupation || !districtId || !interest) throw new Error('所有欄位皆為必填!')
-        if (password !== confirmPassword) throw new Error('密碼與確認密碼不一致!')
+            if (!account || !password || !confirmPassword || !genderId || !birthday || !occupation || !districtId || !interest) throw new Error('所有欄位皆為必填!')
+            if (password !== confirmPassword) throw new Error('密碼與確認密碼不一致!')
 
-        return User.findOne({ where: { account } })
-        .then((userData) => {
+            const userData = await User.findOne({ where: { account } })
             if (userData) throw new Error('重複註冊帳號！')
 
-            return Promise.all([
-                bcrypt.hash(password, 10),
-                Interest.findOne({ where: { name: interest } })
-            ])
-        })
-        .then(([hash, interestData]) => {
-            if (!interestData) { 
-                return Promise.all([
-                    User.create({
-                        account,
-                        password: hash,
-                        genderId,
-                        birthday,
-                        occupation,
-                        districtId
-                    }),
-                    Interest.create({ name: interest })
-                ])
-            }
-            return Promise.all([
-                User.create({
-                    account,
-                    password: hash,
-                    genderId,
-                    birthday,
-                    occupation,
-                    districtId
-                }),
-                interestData
-            ])
-        })
-        .then(([newUser, interestData]) => {
-            return Owned_interest.create({
-                userId: newUser.id,
-                interestId: interestData.id
+            const newUser = await User.create({
+                account,
+                password: bcrypt.hashSync(password, bcrypt.genSaltSync(10), null),
+                genderId,
+                birthday,
+                occupation,
+                districtId
             })
-        })
-        .then(() =>{
+            const interestData = await Interest.findOne({ where: { name: interest } })
+            const userInterest = interestData ? interestData : await Interest.create({ name: interest })
+            const newOwnedInterest = await Owned_interest.create({
+                userId: newUser.id,
+                interestId: userInterest.id
+            })
             req.flash('success_messages', '註冊成功!')
             res.redirect('/users/login')
-        })
-        .catch(err => next(err))
+        } catch(err) {
+            next(err)
+        }
     },
     getUserMessages: (req, res) => {
         res.render('userMessages')
