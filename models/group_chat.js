7fe83a12d99fb1855b8f309bc9c22a47273ebc3b
Liangni <light0104@gmail.com>
2022-04-28 13:57:30 +0800

    Fix group_chat model for incorrect association with user

--- models/group_chat.js
+++ models/group_chat.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      Group_chat.hasMany(models.User, { foreignKey: 'userId' })
+      Group_chat.belongsTo(models.User, { foreignKey: 'userId' })
       Group_chat.belongsToMany(models.User, {
         through: models.Group_register,
         foreignKey: 'groupId',
