67c5d04afc780fcb518f7befdee80aa28d0bf0fb
Liangni <light0104@gmail.com>
2022-04-29 18:18:28 +0800

    Modify header: add groupChats button & userMessages button

--- helpers/handlebars-helpers.js
+++ helpers/handlebars-helpers.js
@@ -1,4 +1,12 @@
 const dayjs = require('dayjs')
 module.exports = {
-  dateFormator: (options) => { return dayjs(options.fn(this)).format('MM/DD/YYYY') }
+  dateFormator: (options) => { return dayjs(options.fn(this)).format('MM/DD/YYYY') },
+  mkArr: (...items) => { 
+    items.pop()
+    return items
+  },
+  ifCond: (a, b, options) => { 
+    if (Array.isArray(b) && b.includes(a)) return options.fn(this)
+    if (a === b) return options.fn(this)
+  }
 }
