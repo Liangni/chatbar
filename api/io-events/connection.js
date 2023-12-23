0455078230c57f261b6eb8ced0a0d122f9f7da69
Penny Pan <pennypan@Pennys-MacBook-Air.local>
2023-12-23 12:32:20 +0800

    fix eslint style for files under api/io-events

--- api/io-events/connection.js
+++ api/io-events/connection.js
@@ -1,26 +1,26 @@
-const redisConnect = require('../../utility/redis');
+const redisConnect = require('../../utility/redis')
 
 const onConnection = (io, socketEvents) => async (socket) => {
-  const { user } = socket.request;
-  const userGroupChatIds = user.RegisteredGroups?.map((group) => group.id) || [];
+    const { user } = socket.request
+    const userGroupChatIds = user.RegisteredGroups?.map((group) => group.id) || []
 
-  // 確認 redis 是否有前次連線資訊，無則發布「新登入事件」
-  const previousSocketId = await redisConnect.hget('userSocketHash', user.id.toString());
-  if (!previousSocketId) io.emit('newLogin', userGroupChatIds, user.id);
+    // 確認 redis 是否有前次連線資訊，無則發布「新登入事件」
+    const previousSocketId = await redisConnect.hget('userSocketHash', user.id.toString())
+    if (!previousSocketId) io.emit('newLogin', userGroupChatIds, user.id)
 
-  await redisConnect.hset('userSocketHash', user.id.toString(), socket.id);
+    await redisConnect.hset('userSocketHash', user.id.toString(), socket.id)
 
-  // 加入參與的 chatRoom
-  userGroupChatIds.forEach((id) => socket.join(`groupChat${id}`));
+    // 加入參與的 chatRoom
+    userGroupChatIds.forEach((id) => socket.join(`groupChat${id}`))
 
-  // 監聽 socket events
-  Object.keys(socketEvents).forEach((key) => {
-    const fn = socketEvents[key](socket, io);
-    socket.on(key, fn);
-  });
+    // 監聽 socket events
+    Object.keys(socketEvents).forEach((key) => {
+        const fn = socketEvents[key](socket, io)
+        socket.on(key, fn)
+    })
 
-  // 發送 connected event 告知客戶端 server 已準備好
-  io.to(socket.id).emit('connected');
-};
+    // 發送 connected event 告知客戶端 server 已準備好
+    io.to(socket.id).emit('connected')
+}
 
-module.exports = onConnection;
+module.exports = onConnection
