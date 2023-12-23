720bbb7fb3dfedd5d9f4be7e1900907cfa125f1f
Penny Pan <pennypan@Pennys-MacBook-Air.local>
2023-12-23 18:57:22 +0800

    add register test case to user test

--- test/user.test.js
+++ test/user.test.js
@@ -56,3 +56,43 @@ describe('login requests', () => {
         })
     })
 })
+
+// 註冊功能測試
+describe('register request', () => {
+    describe('if user want to register', () => {
+        beforeAll(async () => {
+            // 在測試資料庫新增一個 gender、district
+            await models.Gender.create({})
+            await models.District.create({})
+        })
+
+        // 確認可以顯示 GET /pages/register 的頁面
+        test('should render the register page', async () => {
+            const response = await request(app).get('/pages/register')
+            expect(response.status).toBe(200)
+        })
+
+        test('should register successfully', async() => {
+            const birthday = new Date(1990, 0, 1)
+            const isoFormattedBirthday = birthday.toISOString()
+
+            const response = await request(app)
+                .post('/users/register')
+                .send(`account=User1&password=User1&confirmPassword=User1&genderId=1&birthday=${isoFormattedBirthday}&occupation=User1&districtId=1&interest=User1`)
+                .set('Accept', 'application/json')
+
+            expect(response.status).toBe(302)
+            expect(response.header.location).toBe('/pages/login')
+        })
+
+        afterAll(async () => {
+            await models.sequelize.query('SET FOREIGN_KEY_CHECKS = 0', null, { raw: true })
+            await models.User.destroy({ where: {}, truncate: true, force: true })
+            await models.Gender.destroy({ where: {}, truncate: true, force: true })
+            await models.District.destroy({ where: {}, truncate: true, force: true })
+            await models.Interest.destroy({ where: {}, truncate: true, force: true })
+            await models.Owned_interest.destroy({ where: {}, truncate: true, force: true })
+            await models.sequelize.query('SET FOREIGN_KEY_CHECKS = 1', null, { raw: true })
+        })
+    })
+})
