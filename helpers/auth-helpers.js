77566f45a86e0700039791ca6bb20a59cfbbb615
Penny Pan <pennypan@Pennys-MacBook-Air.local>
2023-12-23 12:37:06 +0800

    fix eslint style for files under helpers

--- helpers/auth-helpers.js
+++ helpers/auth-helpers.js
@@ -1,10 +1,10 @@
 //  成功登入的狀況下，Passport會回傳user，可以透過req.user 把user物件拿出來
-const getUser = (req) => req.user || null;
+const getUser = (req) => req.user || null
 
 //   Passport提供的函式，會根據request的登入狀態回傳 true 或 false
-const ensureAuthenticated = (req) => req.isAuthenticated(req);
+const ensureAuthenticated = (req) => req.isAuthenticated(req)
 
 module.exports = {
-  getUser,
-  ensureAuthenticated
-};
+    getUser,
+    ensureAuthenticated
+}
--- helpers/handlebars-helpers.js
+++ helpers/handlebars-helpers.js
@@ -1,28 +1,27 @@
-/* eslint-disable consistent-return */
-const dayjs = require('dayjs');
+const dayjs = require('dayjs')
 
 module.exports = {
-  dateFormator: (options) => dayjs(options.fn(this).toLocaleString('zh-TW', { timeZone: 'Asia/Taipei' })).format('MM/DD/YYYY'),
-  makeArray: (...items) => {
-    items.pop();
-    return items;
-  },
-  some: (a, arr, options) => {
-    if (arr.some((item) => a === item)) return options.fn(this);
-  },
-  ifCond: (a, b, options) => {
-    if (a === b) return options.fn(this);
-  },
-  getFileNameFromUrl: (options) => {
-    const indexes = [];
-    let index = options.fn(this).indexOf('/');
+    dateFormator: (options) => dayjs(options.fn(this).toLocaleString('zh-TW', { timeZone: 'Asia/Taipei' })).format('MM/DD/YYYY'),
+    makeArray: (...items) => {
+        items.pop()
+        return items
+    },
+    some: (a, arr, options) => {
+        if (arr.some((item) => a === item)) return options.fn(this)
+    },
+    ifCond: (a, b, options) => {
+        if (a === b) return options.fn(this)
+    },
+    getFileNameFromUrl: (options) => {
+        const indexes = []
+        let index = options.fn(this).indexOf('/')
 
-    while (index !== -1) {
-      indexes.push(index);
-      index = options.fn(this).indexOf('/', index + 1);
+        while (index !== -1) {
+            indexes.push(index)
+            index = options.fn(this).indexOf('/', index + 1)
+        }
+        const fileNameIndexStart = indexes[indexes.length - 1] + 1
+        const fileName = options.fn(this).slice(fileNameIndexStart, options.fn(this).length)
+        return fileName
     }
-    const fileNameIndexStart = indexes[indexes.length - 1] + 1;
-    const fileName = options.fn(this).slice(fileNameIndexStart, options.fn(this).length);
-    return fileName;
-  }
-};
+}
