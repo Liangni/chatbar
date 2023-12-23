d48aece07973564e3e14b8651ae71519da22a621
Penny Pan <pennypan@Pennys-MacBook-Air.local>
2023-12-23 12:57:47 +0800

    fix eslint style for sockets.js

--- sockets.js
+++ sockets.js
@@ -1,21 +1,20 @@
-/* eslint-disable no-param-reassign */
-const { wrap, authenticate } = require('./middleware/sockets');
-const sessionMiddleware = require('./middleware/session-middleware');
-const passport = require('./config/passport');
-const onConnection = require('./api/io-events/connection');
-const socketEvents = require('./api/socket-events');
+const { wrap, authenticate } = require('./middleware/sockets')
+const sessionMiddleware = require('./middleware/session-middleware')
+const passport = require('./config/passport')
+const onConnection = require('./api/io-events/connection')
+const socketEvents = require('./api/socket-events')
 
 function init(io) {
-  io.use(wrap(sessionMiddleware));
-  io.use(wrap(passport.initialize()));
-  io.use(wrap(passport.session()));
-  io.use(authenticate);
+    io.use(wrap(sessionMiddleware))
+    io.use(wrap(passport.initialize()))
+    io.use(wrap(passport.session()))
+    io.use(authenticate)
 }
 
 function listen(io) {
-  init(io);
+    init(io)
 
-  io.on('connection', onConnection(io, socketEvents));
+    io.on('connection', onConnection(io, socketEvents))
 }
 
-module.exports = { listen };
+module.exports = { listen }
