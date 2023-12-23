455491cd21222654efb4a1681fd189c664c2b7ed
Penny Pan <pennypan@Pennys-MacBook-Air.local>
2023-12-23 12:57:16 +0800

    fix eslint style for server.js

--- server.js
+++ server.js
@@ -1,28 +1,26 @@
-/* eslint-disable no-console */
-/* eslint-disable global-require */
-require('dotenv').config();
+require('dotenv').config()
 
-const http = require('http');
-const io = require('socket.io');
-const redisConnect = require('./utility/redis');
+const http = require('http')
+const io = require('socket.io')
+const redisConnect = require('./utility/redis')
 
-const { REDIS_PASSWORD, REDIS_HOST, REDIS_PORT } = process.env;
+const { REDIS_PASSWORD, REDIS_HOST, REDIS_PORT } = process.env
 
 redisConnect.init({
-  password: REDIS_PASSWORD,
-  host: REDIS_HOST,
-  port: REDIS_PORT
-});
+    password: REDIS_PASSWORD,
+    host: REDIS_HOST,
+    port: REDIS_PORT
+})
 
-const apiServer = require('./app');
+const apiServer = require('./app')
 
-const httpServer = http.createServer(apiServer);
-const socketServer = io(httpServer);
+const httpServer = http.createServer(apiServer)
+const socketServer = io(httpServer)
 
-const sockets = require('./sockets');
+const sockets = require('./sockets')
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 httpServer.listen(PORT, () => {
-  console.log(`App is running on port ${PORT}!`);
-});
-sockets.listen(socketServer);
+    console.log(`App is running on port ${PORT}!`)
+})
+sockets.listen(socketServer)
