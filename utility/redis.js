c07ec1b9d8ebb0a79752cf19065a43c3f4db012c
Penny Pan <pennypan@Pennys-MacBook-Air.local>
2023-12-23 12:55:13 +0800

    fix eslint style for files under utility/

--- utility/redis.js
+++ utility/redis.js
@@ -1,58 +1,57 @@
-/* eslint-disable import/no-extraneous-dependencies */
-const Redis = require('ioredis');
+const Redis = require('ioredis')
 
-let client;
+let client
 const redisConnect = {
-  init({ password, host, port }) {
-    client = new Redis({
-      host,
-      password,
-      port
-    });
-  },
-  async get(key) {
-    if (!key) return null;
-    const val = await client.get(key);
-    return val;
-  },
-  async set(key, value = '1', options = {}) {
-    if (!value) return;
+    init({ password, host, port }) {
+        client = new Redis({
+            host,
+            password,
+            port
+        })
+    },
+    async get(key) {
+        if (!key) return null
+        const val = await client.get(key)
+        return val
+    },
+    async set(key, value = '1', options = {}) {
+        if (!value) return
 
-    let { cacheTime } = options;
-    cacheTime = cacheTime || 60 * 60 * 10;
+        let { cacheTime } = options
+        cacheTime = cacheTime || 60 * 60 * 10
 
-    await client.set(key, value, 'EX', cacheTime);
-  },
-  async hget(key, field) {
-    if (!key || !field) return null;
+        await client.set(key, value, 'EX', cacheTime)
+    },
+    async hget(key, field) {
+        if (!key || !field) return null
 
-    const val = await client.hget(key, field);
-    return val;
-  },
-  async hset(key, field, val) {
-    if (!key || !field) return;
+        const val = await client.hget(key, field)
+        return val
+    },
+    async hset(key, field, val) {
+        if (!key || !field) return
 
-    await client.hset(key, field, val);
-  },
-  async hgetall(key) {
-    if (!key) return;
+        await client.hset(key, field, val)
+    },
+    async hgetall(key) {
+        if (!key) return
 
-    const result = await client.hgetall(key);
-    // eslint-disable-next-line consistent-return
-    return result;
-  },
-  async hkeys(key) {
-    if (!key) return;
+        const result = await client.hgetall(key)
+        // eslint-disable-next-line consistent-return
+        return result
+    },
+    async hkeys(key) {
+        if (!key) return
 
-    const fields = await client.hkeys(key);
-    // eslint-disable-next-line consistent-return
-    return fields;
-  },
-  async hdel(key, field) {
-    if (!key || !field) return;
+        const fields = await client.hkeys(key)
+        // eslint-disable-next-line consistent-return
+        return fields
+    },
+    async hdel(key, field) {
+        if (!key || !field) return
 
-    await client.hdel(key, field);
-  }
-};
+        await client.hdel(key, field)
+    }
+}
 
-module.exports = redisConnect;
+module.exports = redisConnect
