d9b8a59df6faa667abfffbb6b3f0b5717893e440
Penny Pan <pennypan@Pennys-MacBook-Air.local>
2023-12-26 12:05:17 +0800

    add reciever delete friendshipInvitations test

--- test/friendship.test.js
+++ test/friendship.test.js
@@ -102,41 +102,84 @@ describe('friendship request', () => {
             })
         })
 
-        describe('sender delete friendshipInvitations', () => {
-            beforeAll(async () => {
-                // 模擬登入資料
-                authHelpers.ensureAuthenticated.mockReturnValue(true)
-                authHelpers.getUser.mockReturnValue({ id: 1 })
+        describe('delete friendshipInvitations', () => {
+            describe('sender delete friendshipInvitations', () => {
+                beforeAll(async () => {
+                    // 模擬登入資料
+                    authHelpers.ensureAuthenticated.mockReturnValue(true)
+                    authHelpers.getUser.mockReturnValue({ id: 1 })
+        
+                    // 在測試資料庫中，新增 mock 資料
+                    await models.User.bulkCreate([
+                        { id: 1, account: 'User1' },
+                        { id: 2, account: 'User2' }
+                    ])
+                    await models.Friendship_invitation.create({ senderId: 1, recieverId: 2 })
+                })
     
-                // 在測試資料庫中，新增 mock 資料
-                await models.User.bulkCreate([
-                    { id: 1, account: 'User1' },
-                    { id: 2, account: 'User2' }
-                ])
-                await models.Friendship_invitation.create({ senderId: 1, recieverId: 2 })
+                test('DELETE /friendships/users/:userId/friendshipInvitations', async() => {
+                    const response = await request(app)
+                        .post('/friendships/users/2/friendshipInvitations')
+                        .set('Accept', 'application/json')
+    
+                    expect(response.status).toBe(302)
+                })
+    
+                test('should create current user friendship invitation', async () => {
+                    const friendshipInvitation = await models.Friendship_invitation.findOne({ where: { senderId: 1, recieverId: 2 } })
+                    expect(friendshipInvitation).not.toBeNull()
+                })
+    
+                afterAll(async () => {
+                    jest.resetAllMocks()
+                    await models.sequelize.query('SET FOREIGN_KEY_CHECKS = 0', null, { raw: true })
+                    await models.User.destroy({ where: {}, truncate: true, force: true })
+                    await models.Friendship_invitation.destroy({ where: {}, truncate: true, force: true })
+                    await models.sequelize.query('SET FOREIGN_KEY_CHECKS = 1', null, { raw: true })
+                })
             })
 
-            test('DELETE /friendships/users/:userId/friendshipInvitations', async() => {
-                const response = await request(app)
-                    .post('/friendships/users/2/friendshipInvitations')
-                    .set('Accept', 'application/json')
-
-                expect(response.status).toBe(302)
+            describe('reciever delete friendshipInvitations', () => {
+                beforeAll(async () => {
+                    // 模擬登入資料
+                    authHelpers.ensureAuthenticated.mockReturnValue(true)
+                    authHelpers.getUser.mockReturnValue({ id: 2 })
+        
+                    // 在測試資料庫中，新增 mock 資料
+                    await models.User.bulkCreate([
+                        { id: 1, account: 'User1' },
+                        { id: 2, account: 'User2' }
+                    ])
+                    await models.Friendship_invitation.create({ senderId: 1, recieverId: 2 })
+                })
+    
+                test('DELETE /friendships/users/:userId/friendshipInvitations', async() => {
+                    const response = await request(app)
+                        .post('/friendships/users/1/friendshipInvitations')
+                        .set('Accept', 'application/json')
+    
+                    expect(response.status).toBe(302)
+                })
+    
+                test('should create current user friendship invitation', async () => {
+                    const friendshipInvitation = await models.Friendship_invitation.findOne({ where: { senderId: 1, recieverId: 2 } })
+                    expect(friendshipInvitation).not.toBeNull()
+                })
+    
+                afterAll(async () => {
+                    jest.resetAllMocks()
+                    await models.sequelize.query('SET FOREIGN_KEY_CHECKS = 0', null, { raw: true })
+                    await models.User.destroy({ where: {}, truncate: true, force: true })
+                    await models.Friendship_invitation.destroy({ where: {}, truncate: true, force: true })
+                    await models.sequelize.query('SET FOREIGN_KEY_CHECKS = 1', null, { raw: true })
+                })
             })
 
-            test('should create current user friendship invitation', async () => {
-                const friendshipInvitation = await models.Friendship_invitation.findOne({ where: { senderId: 1, recieverId: 2 } })
-                expect(friendshipInvitation).not.toBeNull()
-            })
-
-            afterAll(async () => {
-                jest.resetAllMocks()
-                await models.sequelize.query('SET FOREIGN_KEY_CHECKS = 0', null, { raw: true })
-                await models.User.destroy({ where: {}, truncate: true, force: true })
-                await models.Friendship_invitation.destroy({ where: {}, truncate: true, force: true })
-                await models.sequelize.query('SET FOREIGN_KEY_CHECKS = 1', null, { raw: true })
-            })
+            
+            
         })
+
+
 
     })
 })
